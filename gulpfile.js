dd7fee6e183c319dc1d63d2441e20f0049f30794
Marco Biedermann <marco.biedermann@gmx.de>
2016-01-29 10:34:33 +0100

    add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var mincss = require('gulp-minify-css');
 var addsrc = require('gulp-add-src');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
 var del = require("del");
 
 gulp.task('clean', function(){
@@ -20,6 +22,11 @@ gulp.task('dist', function(){
     .pipe(concat('voxelcss.js'))
     .pipe(gulp.dest('./dist'));
   gulp.src('./src/voxel.css')
+    .pipe(postcss([
+      autoprefixer({
+        browser: ['IE 8']
+      })
+    ]))
     .pipe(mincss({compatibility: 'ie8'}))
     .pipe(gulp.dest('./dist'));
 });
